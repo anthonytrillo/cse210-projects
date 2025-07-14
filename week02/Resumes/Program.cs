64d0999d8c048f67c276d529aae6484500babc37
anthonytrillo <anthony.trillo.92@gmail.com>
2025-07-13 21:56:33 -0300

    Added learning activity Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Job job1 = new Job
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
-    }
+      _company = "Google",
+      _jobTitle = "Software Engineer",
+      _startYear = 2015,
+      _endYear = 2023
+    };
+
+    Job job2 = new Job
+    {
+      _company = "Meta",
+      _jobTitle = "Technical Lead",
+      _startYear = 2023,
+      _endYear = 2024
+    };
+
+    Resume myResume = new Resume
+    {
+      _name = "Anthony Trillo"
+    };
+
+    myResume._jobs.Add(job1);
+    myResume._jobs.Add(job2);
+    myResume.Display();
+  }
 }
